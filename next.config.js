967e9da4bfa425d91a2e6527eb4d4e22e0a4d290
Pankaj sharma <sharmapankaj01234@hotmail.com>
2023-06-18 17:37:03 +0530

    Add manifest to nextjs application

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+module.exports = {
+	reactStrictMode: true,
+};
+const withPWA = require('next-pwa')({
+    dest: 'public'
+  })
+  
+  module.exports = withPWA({
+    // next.js config
+  })
--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#90cdf4" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
